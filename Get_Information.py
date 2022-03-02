1dcedf750837e5be8ebfd0905c6db723b23fd815
The Great Depression <89604800+PanicCoder@users.noreply.github.com>
2022-03-02 17:40:32 +0100

    Delete Get_Information.py

--- Get_Information.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-from PIL import Image
-
-file_name = "grosser-baer.png"
-urllib.request.urlretrieve("https://astrokramkiste.de/images/sternbilder/grosser-baer-a.png",file_name)
-Image.open(file_name).save(".\\Images\\"+file_name+".png")
-
