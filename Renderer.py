bbd404078106351ab9177ee33d2f5302f9dfa345
The Great Depression <89604800+PanicCoder@users.noreply.github.com>
2022-03-07 15:18:39 +0100

    Add files via upload

--- Renderer.py
+++ Renderer.py
@@ -7,40 +7,7 @@ from Lines import Line
 from Texts import Text
 from Buttons import Buttons
 from Images import Image
-
-
-class In_common():
-
-    def __init__(self, object_) -> None:
-        self.object = object_
-        self.screen = pygame.display.get_surface()
-        self.old_dimensions = (self.screen.get_width(),self.screen.get_height())
-
-    def repaint(self, list_to_draw:List):
-        #repaints all stored objects
-        self.check_resize((self.screen.get_width(),self.screen.get_height()))
-        for list in list_to_draw:
-            for element in list:
-                element.draw()
-
-        pygame.display.update()
-
-    def check_resize(self, new_dimensions:Tuple[int,int]):
-        if self.old_dimensions[0] != new_dimensions[0] or self.old_dimensions[1] != new_dimensions[1]:
-            self.object.__init__()
-            self.old_dimensions = new_dimensions
-            self.object.repaint()
-
-    def check_collision(self, collion_list:List, old_pos):
-        Collisions = []
-        for stars in collion_list:
-            Collisions.append(stars.check_collision(old_pos))
-        
-        for element in Collisions:
-            if element[0]:
-                return element
-
-        return (False,None)
+from Interface import In_common
 
 
 class Game_Render():
@@ -121,7 +88,6 @@ class Game_Render():
 
     def check_resize(self, new_dimensions:Tuple[int,int]):
         self.in_common.check_resize(new_dimensions)
-
     
 class Game_Lobby():
 
